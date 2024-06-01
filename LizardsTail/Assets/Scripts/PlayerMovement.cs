98fce2d0c6c97df86eddee15414ece0e46d595a3
Jaden Lawrence <jaden2.lawrence@live.uwe.ac.uk>
2024-06-01 19:39:01 +0100

    Player controller
    Added a player movement script which allows the player to move left and right, and jump. Also added a raycast to the player to check if it is grounded

---
+++ LizardsTail/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private Vector2 jumpHeight;
+    public Vector2 boxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+
+        // left/right
+        Vector3 direction = new Vector2(horizontalInput, 0);
+
+        transform.Translate(direction * speed * Time.deltaTime);
+        
+        if (horizontalInput < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (horizontalInput > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float jumpInput = Input.GetAxis("Jump");
+
+        // jumping
+        if (isGrounded() && jumpInput > 0)
+        {
+            GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+
+    bool isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+    }
+}
